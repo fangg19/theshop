1d9c541388f2f1a07abcec05d05f7dff2e028f6a
fangg19 <alexjohnny6@gmail.com>
2021-01-23 17:38:05 +0200

    created the routes for the API

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema(
   {
-    user: {
+    name: {
       type: String,
       required: true,
     },
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema(
       required: true,
       unique: true,
     },
-    paswword: {
+    password: {
       type: String,
       required: true,
     },
